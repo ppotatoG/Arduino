70370ca8ba3d7c75597319147732d20306d6b9c6
ppotatoG <thgus2270@gmail.com>
2021-04-23 18:16:47 +0900

    sw_01 click, Sparkle

--- example01/js/event.js
+++ example01/js/event.js
@@ -11,9 +11,16 @@ function addList(t){
 
 const sw_01 = document.querySelector('button[name=switch01]')
 const sw_02 = document.querySelector('button[name=switch02]')
+const LED = document.querySelector('.LED')
 
 sw_01.addEventListener('click', function(){
     addList('sw_01 클릭')
+    LED.classList.add('duration')
+    LED.classList.add('Sparkle')
+    setTimeout(function(){
+        LED.classList.remove('duration')
+        LED.classList.remove('Sparkle')
+    }, 1000)    
 })
 
 sw_02.addEventListener('click', function(){
