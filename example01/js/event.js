c8b6e4ae222aaf2c6a993bbe90e84a6b419680ce
ppotatoG <thgus2270@gmail.com>
2021-04-26 17:57:07 +0900

    target으로 잡는건 안좋은 방법인 것 같다

--- example01/js/event.js
+++ example01/js/event.js
@@ -9,27 +9,51 @@ function addList(t){
     liNode.appendChild(textNode)
 }
 
-const sw_01 = document.querySelector('button[name=switch01]')
-const sw_02 = document.querySelector('button[name=switch02]')
-const LED = document.querySelector('.LED')
+const warp = document.querySelector("#wrap");
+const sw_01 = document.querySelector('button[name=switch01]');
+const sw_02 = document.querySelector('button[name=switch02]');
+const LED = document.querySelector('.LED');
 let clickTime = 0;
 
-sw_01.addEventListener('click', function(){
-    clickTime++;
-    
-    addList('sw_01 '+clickTime+'번 클릭')
+// sw_01.addEventListener('click', function(){
+//     clickTime++;
 
-    if(clickTime > 2){
-        LED.classList.add('duration')
-        LED.classList.add('sparkle')
-        setTimeout(function(){
-            LED.classList.remove('duration')
-            LED.classList.remove('sparkle')
-            addList('sw_01 '+clickTime+'번 클릭 종료')
-        }, 1000)
+//     addList('sw_01 '+clickTime+'번 클릭')
+
+//     if(clickTime > 2){
+//         LED.classList.add('duration')
+//         LED.classList.add('sparkle')
+//         setTimeout(function(){
+//             LED.classList.remove('duration')
+//             LED.classList.remove('sparkle')
+//             addList('sw_01 '+clickTime+'번 클릭 종료')
+//         }, 1000)
+//     }    
+// })
+
+
+// sw_02.addEventListener('click', function(){
+//     addList('sw_02 클릭')
+// })
+
+warp.addEventListener('click', function(e){
+    if(e.target.name =="switch01"){
+        clickTime++;
+        addList('sw_01 '+clickTime+'번 클릭')
+        console.log('e.target.name =="switch01"')
+        if(clickTime >= 3){
+            LED.classList.add('duration')
+            LED.classList.add('sparkle')
+
+            console.log('clickTime >= 3')
+
+            setTimeout(function(){
+                LED.classList.remove('duration')
+                LED.classList.remove('sparkle')
+                addList('sw_01 '+clickTime+'번 클릭 종료')
+            }, 1000)
+            
+            console.log(e.target)
+        } else false
     }
-})
-
-sw_02.addEventListener('click', function(){
-    addList('sw_02 클릭')
 })
