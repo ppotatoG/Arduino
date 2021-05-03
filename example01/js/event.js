e555a20f1061938c909c708bffc1bf55b716d089
ppotatoG <thgus2270@gmail.com>
2021-05-03 18:17:33 +0900

    sw1Fnc clear

--- example01/js/event.js
+++ example01/js/event.js
@@ -27,28 +27,38 @@ function clickFnc(){
         addList('btn1 ' + clickTime + '번 클릭')
     }
 } 
-let sw1Fnc
-let sw2Fnc
 function LED(){
-    LEDBox.classList.add('duration')
-    LEDBox.classList.add('sparkle')
-
-    setTimeout(function(){
-        LEDBox.classList.remove('duration')
-        LEDBox.classList.remove('sparkle')
-    }, 1000)
+    LEDfnc1()
+    sw2Fnc = setInterval(LEDfnc1, 2000);
 
     sw_02.addEventListener('click', function(){
-        addList('btn2 click')
-        LEDfnc2();
-        sw2Fnc = setInterval(LEDfnc2, 4000);
+        clearInterval(sw1Fnc)
+        // addList('btn2 click')
+        // LEDfnc2();
+        // sw2Fnc = setInterval(LEDfnc2, 4000);
     }, {once: true})
 }
 
-let conut = 0;
+let sw1Fnc;
+let sw2Fnc;
+let conut1 = 0;
+let conut2 = 0;
+
+function LEDfnc1(){
+    conut1++
+    addList('01 '+conut1+'번째 setInterval')
+
+    LEDBox.classList.add('duration')
+    LEDBox.classList.add('sparkle')
+    setTimeout(function(){
+        addList('setTimeout')
+        LEDBox.classList.remove('duration')
+        LEDBox.classList.remove('sparkle')
+    }, 1000)
+}
 function LEDfnc2(){
-    conut++
-    addList(conut+'번째 setInterval')
+    conut2++
+    addList(conut2+'번째 setInterval')
 
     LEDBox.classList.add('duration')
     LEDBox.classList.add('sparkle')
@@ -58,7 +68,7 @@ function LEDfnc2(){
         LEDBox.classList.remove('sparkle')
     }, 2000)
 
-    if(conut > 4) {
+    if(conut2 > 4) {
         addList("clearInterval")
         clearInterval(sw2Fnc)
     }
