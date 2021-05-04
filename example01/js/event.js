bcf09d4c12cbfee710ea0ea6fcba7cab71e01a57
ppotatoG <thgus2270@gmail.com>
2021-05-04 16:17:03 +0900

    addClass, removeClass 추가 공백 추가

--- example01/js/event.js
+++ example01/js/event.js
@@ -13,11 +13,15 @@ const LEDBox = document.querySelector('.LED')
 const sw_02 = document.querySelector('button[name=switch02]')
 
 let clickTime = 0;
+let sw1Fnc;
+let sw2Fnc;
+let conut1 = 0;
+let conut2 = 0;
 
 function clickFnc(){
     clickTime++;
     if(clickTime == 3) {
-        addList('glwLED() 실행')
+        addList('LED() 실행')
         LED()
     }
     else if(clickTime > 3 ) {
@@ -27,51 +31,50 @@ function clickFnc(){
         addList('btn1 ' + clickTime + '번 클릭')
     }
 } 
+
 function LED(){
     LEDfnc1()
     sw1Fnc = setInterval(LEDfnc1, 2000);
 
     sw_02.addEventListener('click', function(){
         clearInterval(sw1Fnc)
-        addList("sw1Fnc clearInterval")
+        addList("sw1Fnc End")
+
+        LEDBox.classList.remove('duration')
+        LEDBox.classList.remove('sparkle')
         
-        addList('btn2 click')
+        addList('btn2 클릭')
         LEDfnc2();
         sw2Fnc = setInterval(LEDfnc2, 4000);
     }, {once: true})
 }
 
-let sw1Fnc;
-let sw2Fnc;
-let conut1 = 0;
-let conut2 = 0;
-
 function LEDfnc1(){
     conut1++
-    addList('01 '+conut1+'번째 setInterval')
+    addList('LEDfnc1 '+conut1+'번째 start')
 
     LEDBox.classList.add('duration')
     LEDBox.classList.add('sparkle')
     setTimeout(function(){
-        addList('setTimeout')
+        addList('LEDfnc1 '+conut1+'번째 end')
         LEDBox.classList.remove('duration')
         LEDBox.classList.remove('sparkle')
     }, 1000)
 }
 function LEDfnc2(){
     conut2++
-    addList(conut2+'번째 setInterval')
+    addList('LEDfnc2 '+conut2+'번째 start')
 
     LEDBox.classList.add('duration')
     LEDBox.classList.add('sparkle')
     setTimeout(function(){
-        addList('setTimeout')
+        addList('LEDfnc2 '+conut2+'번째 end')
         LEDBox.classList.remove('duration')
         LEDBox.classList.remove('sparkle')
     }, 2000)
 
     if(conut2 > 4) {
-        addList("clearInterval")
+        addList("sw2Fnc End")
         clearInterval(sw2Fnc)
     }
 }
